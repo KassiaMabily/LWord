7ba088a7704f55e4c9ff340a0a685647c65b0b96
Kassia Fraga <kassiafraga7@gmail.com>
2022-05-22 14:45:53 -0300

    First commit

---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static LWord.Utils;
+
+namespace LWord
+{
+    public partial class Form1 : Form
+    {
+        private OpenFileDialog openFileDialog = new OpenFileDialog();
+        private Node[] dictionary = new Node[Utils.DICTIONARY_SIZE];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.richTextBox1.Text = "111 Olá mundo!!";
+
+
+            // Utils.HighlightText(this.richTextBox1, "Hello", Color.Red);
+
+            dictionary = Utils.ReadDictionary();
+
+            LoadText(this.richTextBox1.ToString());
+        }
+
+        private void LoadText(string text)
+        {
+            string[] words = text.Split(" ");
+
+            for (int i = 2; i < words.Length; i++)
+            {
+                CheckWord(words[i], dictionary, this.richTextBox1);
+            }
+        }
+
+
+
+
+        private void abrirToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Console.WriteLine("Entrei");
+            }
+        }
+
+
+        private void richTextBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            if (e.Button == MouseButtons.Right)
+            {
+                Point point = new Point(e.X, e.Y);
+                int index = richTextBox1.GetCharIndexFromPosition(point);
+
+                int length = 1;
+
+                if (!Char.IsWhiteSpace(richTextBox1.Text[index]))
+                {
+                    while (index > 0 && !Char.IsWhiteSpace(richTextBox1.Text[index - 1]))
+                    { 
+                        index--; length++; 
+                    }
+
+                    while (index + length < richTextBox1.Text.Length &&
+                        !Char.IsWhiteSpace(richTextBox1.Text[index + length]) &&
+                        (!Char.IsPunctuation(richTextBox1.Text[index + length]) ||
+                        richTextBox1.Text[index + length] == Char.Parse("'"))
+                    )
+                    {
+                        length++;
+                    }
+
+                    richTextBox1.SelectionStart = index;
+                    richTextBox1.SelectionLength = length;
+
+                    if(richTextBox1.SelectionColor == Color.Red)
+                    {
+                        Debug.WriteLine(string.Format("Deseja adicionar a palavra {0} no dicionário?", richTextBox1.SelectedText.ToLower()));
+
+                        DialogResult res = MessageBox.Show(string.Format("Deseja adicionar a palavra {0} no dicionário?", richTextBox1.SelectedText.ToLower()), "Confirmation", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                        if (res == DialogResult.OK)
+                        {
+                            MessageBox.Show("You have clicked Ok Button");
+                            //Some task…
+                        }
+                        if (res == DialogResult.Cancel)
+                        {
+                            MessageBox.Show("You have clicked Cancel Button");
+                            //Some task…
+                        }
+                    }
+                    else
+                    {
+                        richTextBox1.DeselectAll();
+                    }
+
+                }
+            }
+            
+        }
+
+        private void richTextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            // Se o usuário digitar enter ou espaço, então é uma nova palavra
+            if(e.KeyValue == 32 || e.KeyValue == 13)
+            {
+                string[] words = richTextBox1.Text.Split(" ");
+                string lastWord = words[words.Length - 2];
+
+                CheckWord(lastWord, dictionary, richTextBox1);
+            }
+
+        }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace LWord
+{
+    public static class Utils
+    {
+        public static int DICTIONARY_SIZE = 10;
+
+        public static void HighlightText(this RichTextBox myRtb, string word, Color color)
+        {
+
+            if (word == string.Empty)
+                return;
+
+            int s_start = myRtb.SelectionStart, startIndex = 0, index;
+
+            while ((index = myRtb.Text.IndexOf(word, startIndex)) != -1)
+            {
+                myRtb.Select(index, word.Length);
+                myRtb.SelectionColor = color;
+
+                startIndex = index + word.Length;
+            }
+
+            myRtb.SelectionStart = s_start;
+            myRtb.SelectionLength = 0;
+            myRtb.SelectionColor = Color.Black;
+        }
+
+        public static int HashFunction(string s, int size)
+        {
+            int total = 0;
+            char[] c;
+
+            // c = s.ToLower().Normalize(NormalizationForm.FormD).ToCharArray();
+
+            c = s.ToLower().ToCharArray();
+
+            for (int k = 0; k <= c.GetUpperBound(0); k++)
+                total += (int)c[k];
+
+            return total % size;
+        }
+
+        public static Node[] ReadDictionary()
+        {
+            
+            Node[] dictionary = new Node[DICTIONARY_SIZE];
+
+            string file = "dictionary.txt";
+
+            // Verifica se o arquivo não existe
+            if (!File.Exists(file))
+            {
+                Debug.WriteLine(String.Format("Arquivo {0} não existe", file));
+                return dictionary;
+            }
+
+            string[] lines = File.ReadAllLines(file);
+            foreach (var line in lines)
+            {
+                int hashCode = HashFunction(line, DICTIONARY_SIZE);
+                if (dictionary[hashCode] == null)
+                {
+                    Node newNode = new Node(line, null, null);
+                    dictionary[hashCode] = newNode;
+                }
+
+                else
+                {
+                    dictionary[hashCode].insertOrdenate(line);
+                }
+                
+            }
+
+            return dictionary;
+        }
+
+        public static string NormalizeString(string text)
+        {
+            foreach (var chr in new string[] { "(", ")", "!", "@", "#", "[", "]", "?" })
+            {
+                text = text.Replace(chr, "");
+            }
+
+            return text;
+        }
+
+        public static string RemoveDiacritics(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder(capacity: normalizedString.Length);
+
+            for (int i = 0; i < normalizedString.Length; i++)
+            {
+                char c = normalizedString[i];
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder
+                .ToString()
+                .Normalize(NormalizationForm.FormC);
+        }
+
+
+        public static void CheckWord(string word, Node[] dictionary, RichTextBox richTextBox)
+        {
+            // Ignorar textos que forem digitos
+            if (!int.TryParse(word, out _))
+            {
+                string normalizedWord = NormalizeString(RemoveDiacritics(word));
+                int hashCode = HashFunction(normalizedWord, DICTIONARY_SIZE);
+
+                if (dictionary[hashCode] == null)
+                {
+                    HighlightText(richTextBox, NormalizeString(word), Color.Red);
+                }
+
+                else
+                {
+                    Node node = dictionary[hashCode].find(normalizedWord);
+
+                    if (node.getElement() == null)
+                    {
+                        HighlightText(richTextBox, NormalizeString(word), Color.Red);
+                    }
+                }
+            }
+        } 
+
+
+    }
+}
